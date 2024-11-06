0d47bc895e87a867fb1aa3d2adde37a0f8ab5e23
Eril Shaji <eril@192.168.1.4>
2024-11-06 12:30:25 +0530

    Creating Payment Service

---
+++ src/test/java/java/com/ecommerce/paymentservice/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.ecommerce.paymentservice.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
